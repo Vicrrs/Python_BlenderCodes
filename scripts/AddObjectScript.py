13b4c7686829dba4a22d8c6d47372958dd70b709
Vicrrs <enricofermi2019@gmail.com>
2023-09-17 12:38:33 -0400

    adiconando objetos

--- scripts/AddObjectScript.py
+++ scripts/AddObjectScript.py
@@ -5,4 +5,21 @@ class TestPanel(bpy.types.Panel):
     bl_idname = "PT_TestPanel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'NewTab'
+    bl_category = 'My 1st Addon'
+    
+    def draw(self,context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.label(text="Add an object", icon='CUBE')
+        row = layout.row()
+        row.operator("mesh.primitive_plane_add")
+    
+def register():
+    bpy.utils.register_class(TestPanel)
+    
+def unregister():
+    bpy.utils.unregister_class(TestPanel)
+
+if __name__ == "__main__":
+    register()
